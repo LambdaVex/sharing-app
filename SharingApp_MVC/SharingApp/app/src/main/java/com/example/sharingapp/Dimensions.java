725492010209db3c2fdcd97e200919a176476a1e
adenno <aliedenno@gmail.com>
2019-10-09 10:41:09 +0200

    what left

---
+++ SharingApp_MVC/SharingApp/app/src/main/java/com/example/sharingapp/Dimensions.java
@@ -0,0 +1,33 @@
+package com.example.sharingapp;
+
+/**
+ * Dimensions class
+ */
+public class Dimensions {
+
+    private String length;
+    private String width;
+    private String height;
+
+    public Dimensions(String length, String width, String height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getDimensions() {
+        return length + " x " + width + " x " + height;
+    }
+}
